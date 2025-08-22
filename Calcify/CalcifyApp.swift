e05e603236c0b69ad7bc55ca2e0a336f42ba3f9e
Nam Nguyễn <namnv@ermsystem.com>
2025-08-22 07:57:28 +0700

    Initial Commit

---
+++ Calcify/CalcifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcifyApp.swift
+//  Calcify
+//
+//  Created by Nam Nguyễn on 22/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CalcifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calcify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calcify
+//
+//  Created by Nam Nguyễn on 22/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
